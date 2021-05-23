cbbf5b6567e6dee10649313f19006ce81625d592
Alpha <alpha0010@users.noreply.github.com>
2021-05-22 23:33:33 -0400

    feat(ios): emit download progress events

---
+++ ios/NetworkHandler.swift
@@ -0,0 +1,110 @@
+class NetworkHandler: NSObject, URLSessionDownloadDelegate {
+    public static let FETCH_EVENT = "FetchEvent"
+    private static let MIN_EVENT_INTERVAL: TimeInterval = 0.15
+
+    private var currentUrl = ""
+    private var destination: String?
+    private let emitter: RCTEventEmitter
+    private var lastEventTime = Date.distantPast
+    private let requestId: Int
+
+    init(requestId: NSNumber, emitter: RCTEventEmitter) {
+        self.emitter = emitter
+        self.requestId = requestId.intValue
+    }
+
+    func fetch(resource: String, config: NSDictionary) -> Void {
+        guard let url = URL(string: resource) else {
+            onFetchError("'\(resource)' is not a url.")
+            return
+        }
+
+        currentUrl = resource
+        destination = config["path"] as? String
+
+        var request = URLRequest(url: url)
+        if let method = config["method"] as? String {
+            request.httpMethod = method
+        }
+        if let body = config["body"] as? String {
+            request.httpBody = body.data(using: .utf8)
+        }
+        if let headers = config["headers"] as? NSDictionary {
+            for (key, value) in headers {
+                if let headerName = key as? String, let headerValue = value as? String {
+                    request.addValue(headerValue, forHTTPHeaderField: headerName)
+                }
+            }
+        }
+
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate: self,
+            delegateQueue: OperationQueue.current
+        )
+        let downloadTask = session.downloadTask(with: request)
+        downloadTask.resume()
+    }
+
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        lastEventTime = Date.distantFuture
+        guard let response = downloadTask.response as? HTTPURLResponse else {
+            onFetchError("Failed to fetch '\(currentUrl)'.")
+            return
+        }
+
+        if let path = destination {
+            let pathUrl = URL(fileURLWithPath: path)
+            try? FileManager.default.removeItem(at: pathUrl)
+            do {
+                try FileManager.default.moveItem(at: location, to: pathUrl)
+            } catch {
+                onFetchError("Failed to save '\(currentUrl)' to '\(path)'.", error)
+                return
+            }
+        }
+
+        self.emitter.sendEvent(withName: NetworkHandler.FETCH_EVENT, body: [
+            "requestId": self.requestId,
+            "state": "complete",
+            "headers": response.allHeaderFields,
+            "ok": response.statusCode >= 200 && response.statusCode < 300,
+            "redirected": false, // TODO: Determine actual value.
+            "status": response.statusCode,
+            "statusText": HTTPURLResponse.localizedString(forStatusCode: response.statusCode),
+            "url": response.url?.absoluteString ?? ""
+        ])
+    }
+
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        onFetchError("Failed to fetch '\(currentUrl)'.", error)
+    }
+
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        let currentTime = Date()
+        guard currentTime.timeIntervalSince(lastEventTime) > NetworkHandler.MIN_EVENT_INTERVAL else {
+            return
+        }
+        lastEventTime = currentTime
+
+        self.emitter.sendEvent(withName: NetworkHandler.FETCH_EVENT, body: [
+            "requestId": requestId,
+            "state": "progress",
+            "bytesRead": totalBytesWritten,
+            "contentLength": totalBytesExpectedToWrite,
+            "done": false
+        ])
+    }
+
+    private func onFetchError(_ msg: String, _ err: Error? = nil) {
+        var message = msg
+        if let errMsg = err?.localizedDescription {
+            message += " " + errMsg
+        }
+        self.emitter.sendEvent(withName: NetworkHandler.FETCH_EVENT, body: [
+            "requestId": self.requestId,
+            "state": "error",
+            "message": message
+        ])
+    }
+}
