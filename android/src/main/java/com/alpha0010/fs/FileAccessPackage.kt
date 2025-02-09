b8a9bc024f456ab627a7bdbcddbdd1f60056d6ad
Shivam <singhshivamkr@gmail.com>
2025-02-09 22:39:53 +0530

    Update file access package with React Native 0.77 support

--- android/src/main/java/com/alpha0010/fs/FileAccessPackage.kt
+++ android/src/main/java/com/alpha0010/fs/FileAccessPackage.kt
@@ -1,31 +1,34 @@
 package com.alpha0010.fs
 
-import com.facebook.react.TurboReactPackage
+import com.facebook.react.BaseReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfo
 import com.facebook.react.module.model.ReactModuleInfoProvider
 
-class FileAccessPackage : TurboReactPackage() {
-  override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
-    return if (name == FileAccessModule.NAME) {
-      FileAccessModule(reactContext)
-    } else {
-      null
-    }
-  }
 
-  override fun getReactModuleInfoProvider() = ReactModuleInfoProvider {
-    mapOf(
-      FileAccessModule.NAME to ReactModuleInfo(
-        _name = FileAccessModule.NAME,
-        _className = FileAccessModule.NAME,
-        _canOverrideExistingModule = false,
-        _needsEagerInit = false,
-        isCxxModule = false,
-        isTurboModule = true
-      )
-    )
-  }
-  }
+class FileAccessPackage : BaseReactPackage() {
+    override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? =
+        if (name == FileAccessModule.NAME) {
+            FileAccessModule(reactContext)
+        } else {
+            null
+        }
+
+    override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
+        return ReactModuleInfoProvider {
+            val moduleInfos: MutableMap<String, ReactModuleInfo> = HashMap()
+            val isTurboModule: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+            moduleInfos[FileAccessModule.NAME] = ReactModuleInfo(
+                FileAccessModule.NAME, // name
+                FileAccessModule.NAME, // className
+                false,  // canOverrideExistingModule
+                false,  // needsEagerInit
+                false, // hasConstants NOTE: This is deprecated but we need it to keep compatability with RN <= 0.72
+                false,  // isCxxModule
+                isTurboModule // isTurboModule
+            )
+            moduleInfos
+        }
+    }
 }
